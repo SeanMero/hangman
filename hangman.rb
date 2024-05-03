1420e3e5949930f6516855804a03b0e43ebb5174
SeanMero <MeroFromVero@gmail.com>
2024-05-03 17:44:13 +0800

    Add word length condition to word selector

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,4 @@
 dictionary = File.readlines('google-10000-english-no-swears.txt')
-word = dictionary[rand(dictionary.length - 1)].chomp
+word = dictionary[rand(dictionary.length - 1)].chomp until word.to_s.length > 4 && word.to_s.length < 13
 
 p word
